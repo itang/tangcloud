b49a0adb836e541a9c33683fba8aecca058862fc
itang <live.tang@gmail.com>
2016-08-08 10:49:30 +0800

    cleanup

--- dictserice/kotlinversion/src/main/kotlin/app/dao/impl/DictLogRepositoryImpl.kt
+++ dictserice/kotlinversion/src/main/kotlin/app/dao/impl/DictLogRepositoryImpl.kt
@@ -2,13 +2,14 @@ package app.dao.impl
 
 import app.dao.DictLogRepository
 import app.dao.entity.DictLogEntity
+import app.util.inTransaction
+import app.util.parse
+import app.util.stringify
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.stereotype.Repository
-
-import app.util.*
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.data.redis.core.StringRedisTemplate
+import org.springframework.stereotype.Repository
 
 
 @Repository
@@ -18,8 +19,8 @@ open class DictLogRepositoryImpl(
         @Autowired private val JSON: ObjectMapper
 ) : DictLogRepository {
 
-    private val DICT_LOG_KEY = "log"
-    private val DICT_LOG_DATA_KEY = "log:data"
+    private val DICT_LOG_KEY = "tc:dict:log"
+    private val DICT_LOG_DATA_KEY = "tc:dict:log:data"
 
     override fun create(dictLogEntity: DictLogEntity): DictLogEntity {
         val v = dictLogEntity.id!!.toString()
