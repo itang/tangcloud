ba631a071892b2607df62e55fb06fc9681f7916a
itang <live.tang@gmail.com>
2017-06-25 17:04:20 +0800

    cleanup

--- dictserice/rust_rocket_version/src/web/webroot.rs
+++ dictserice/rust_rocket_version/src/web/webroot.rs
@@ -6,11 +6,6 @@ pub fn index() -> &'static str {
     "Hello, world! - rustrocketversion-v0.1"
 }
 
-#[get("/ping")]
-pub fn ping() -> &'static str {
-    "pong"
-}
-
 #[error(404)]
 pub fn not_found() -> JSON<Resp<()>> {
     Resp::json_err(404, Some("Resource was not found."), None)
