72d49431f9bbc1e957fd09c053927a26014874de
itang <live.tang@gmail.com>
2017-07-30 11:37:47 +0800

    cleanup

--- dictserice/elixirversion/config/prod.exs
+++ dictserice/elixirversion/config/prod.exs
@@ -15,7 +15,7 @@ config :elixirversion, ElixirversionWeb.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: "example.com", port: 80],
   #cache_static_manifest: "priv/static/manifest.json",
-  cache_static_manifest: "priv/static/cache_manifest.json",
+  cache_static_manifest: "priv/static/cache_manifest.json"
   #server: true
 
 # Do not print debug messages in production
