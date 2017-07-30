1c9777589ae1148e6ccca5b104fdab760b68dff7
itang <live.tang@gmail.com>
2017-07-30 11:50:16 +0800

    cleanup

--- dictserice/elixirversion/lib/elixirversion_web/controllers/PingController.ex
+++ dictserice/elixirversion/lib/elixirversion_web/controllers/PingController.ex
@@ -2,6 +2,6 @@ defmodule ElixirversionWeb.PingController do
   use ElixirversionWeb, :controller
 
   def ping(conn, _params) do
-    json conn, %{message: "pong"}
+    json conn, %{message: "pong", from: "elixirversion"}
   end
 end
